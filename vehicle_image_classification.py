d0618ca4d5ba50ffcfc7fb37c621fe8833e97554
xenono <aurbanczyk0@gmail.com>
2022-12-19 23:23:26 +0100

    Add single prediction images for test purposes, add foundation of a CNN

--- vehicle_image_classification.py
+++ vehicle_image_classification.py
@@ -2,7 +2,40 @@ import sys  # to access the system
 import cv2
 from os import listdir
 from os.path import isfile, join
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 
+# Preprocessing dataset
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+training_set = train_datagen.flow_from_directory(
+    'dataset/training_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_gen = ImageDataGenerator(rescale=1. / 255)
+test_set = train_datagen.flow_from_directory(
+    'dataset/test_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+# Convolution Neural Network
+cnn = tf.keras.models.Sequential()
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=(64, 64, 3)))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Flatten())
 # path = "/home/xenono/AI/AI_Coursework/dataset/cars/"
 # images = [f for f in listdir(path)[:10] if isfile(join(path, f))]
 #
@@ -18,5 +51,3 @@ from os.path import isfile, join
 #     sys.exit() # to exit from all the processes
 
 # cv2.destroyAllWindows() # destroy all windows
-
-

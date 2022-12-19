5af3cc202c811064f6f97817793294e66c69147b
xenono <aurbanczyk0@gmail.com>
2022-12-19 23:22:41 +0100

    Move images to folders, and split datasets into training set and test set using python scripts, preprocess images for training.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Datasets
+* trucks and buses https://www.kaggle.com/datasets/cb2383dd48f236fde8e0b0c3af0ea806dfaac1e8e95c68417f3fdbd9848765b3
+* Cars and bikes https://www.kaggle.com/datasets/utkarshsaxenadn/car-vs-bike-classification-dataset
---
+++ vehicle_image_classification.py
@@ -0,0 +1,22 @@
+import sys  # to access the system
+import cv2
+from os import listdir
+from os.path import isfile, join
+
+# path = "/home/xenono/AI/AI_Coursework/dataset/cars/"
+# images = [f for f in listdir(path)[:10] if isfile(join(path, f))]
+#
+# for img in images:
+#     img = cv2.imread(path + "/" + img, cv2.IMREAD_ANYCOLOR)
+#     cv2.imshow("Sheep", img)
+#     cv2.waitKey(0)
+# # sys.exit()
+# cv2.destroyAllWindows()  # destroy all windows
+# while True:
+#     cv2.imshow("Sheep", img)
+#     cv2.waitKey(0)
+#     sys.exit() # to exit from all the processes
+
+# cv2.destroyAllWindows() # destroy all windows
+
+
